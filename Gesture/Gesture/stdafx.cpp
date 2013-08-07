703a00fc70fe00c7d1538d7dac57fc89b562205d
장일식 <foreverme@naver.com>
2013-08-07 15:19:12 +0900

    Source Create

---
+++ Gesture/Gesture/stdafx.cpp
@@ -0,0 +1,8 @@
+
+// stdafx.cpp : source file that includes just the standard includes
+// Gesture.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
